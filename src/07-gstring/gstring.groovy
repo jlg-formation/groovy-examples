d1c7d4beacbbafc8ca15eaf82169506a49ab2793
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-07-04 18:16:58 +0200

    dev: added == test

--- src/07-gstring/gstring.groovy
+++ src/07-gstring/gstring.groovy
@@ -2,20 +2,25 @@ var str = 'truc'
 
 assert str.getClass().toString() == 'class java.lang.String'
 assert str.getClass() == String.class
-println str.getClass();
+println str.getClass()
 
 var str2 = "truc"
 
 assert str2.getClass().toString() == 'class java.lang.String'
 assert str2.getClass() == String.class
-println str2.getClass();
+println str2.getClass()
 
 var a = 1
 var b = 2
 
-var str3 = "truc ${a+b}"
+var str3 = "truc ${a + b}"
 println "str3 = $str3"
 
 assert str3.getClass().toString() == 'class org.codehaus.groovy.runtime.GStringImpl'
 assert str3.getClass() == org.codehaus.groovy.runtime.GStringImpl.class
-println str3.getClass();
+println str3.getClass()
+
+var str4 = "hello"
+if (str4 == "hello" + "") {
+    println "this is equal"
+}
