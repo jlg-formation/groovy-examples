7f87c703e91200ce0ade37812cec92d8e76a0bb4
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-07-04 15:47:48 +0200

    dev: moved into src & added 04 ex  on multi-method

---
+++ src/04-multi-method/multi.groovy
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals
+
+int method(String arg) {
+    return 1
+}
+
+int method(Object arg) {
+    return 2
+}
+
+Object o = "Object"
+
+// method chosen is string because at runtime, Groovy see that o is a string.
+// at compile time, Java would see o as an object.
+int result = method(o)
+
+assertEquals(1, result)
+
+println "result = " + result
