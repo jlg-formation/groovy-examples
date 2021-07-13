21bf09c7a929910ab0e9452091b249b23479a2ce
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-07-13 15:25:35 +0200

    dev: added grab example

---
+++ src/14-grab.groovy
@@ -0,0 +1,9 @@
+@Grab('io.github.jlguenego:jlg-prime:0.0.1')
+import io.github.jlguenego.prime.Prime
+
+var nbr = 13
+if (Prime.isPrime(nbr)) {
+    println nbr + " is prime."
+    return
+}
+println nbr + " is not prime."
